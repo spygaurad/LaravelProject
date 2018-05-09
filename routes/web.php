a865ce6ab2bc2c726da24fc1b10f62dd323e2051
xpsprasain <xpsprasain@gmail.com>
2018-05-09 22:35:32 +0545

    in web.php last line

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,14 @@
 
 Route::resource('crud', 'CRUDController');
 
-Route::resource('/', ['uses'=>'CustomerController@index']);
+Route::get('/', ['uses'=>'CustomerController@index']);
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post("login", 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+//xpsprasain
