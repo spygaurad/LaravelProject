29b4896526085d8544ed53b0b54dda9aaec327f7
spygaurad <9841suraj1@gmail.com>
2018-05-09 14:51:20 +0545

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::resource('crud', 'CRUDController');
+
+Route::resource('/', ['uses'=>'CustomerController@index']);
+Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post("login", 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
